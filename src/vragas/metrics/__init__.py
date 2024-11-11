3febc4804a45a4ec683c8e06027915867feebf7f
rodrigocaus <rodrigo.caus13@gmail.com>
2024-11-11 18:49:51 -0300

    chore: add new scores on metrics init

--- src/vragas/metrics/__init__.py
+++ src/vragas/metrics/__init__.py
@@ -1,10 +1,16 @@
 from .exact import ExactMatch
 from .soft import SoftMatch
 from .fscore import F1Score
+from .faithfulness_score import FaithfulnessScore
+from .collection import MetricsCollection
 
 
 __all__ = [
     'ExactMatch',
     'SoftMatch',
-    'F1Score'
+    'F1Score',
+    # Semantic Metrics
+    'FaithfulnessScore',
+    # Containers
+    'MetricsCollection',
 ]
